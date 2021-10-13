a71dfd0748bf6bc02ae033a2fa724f1afd827300
vladyslav-fomenko <92302144+vladyslav-fomenko@users.noreply.github.com>
2021-10-13 16:41:49 +0300

    dynamic ESM imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
-function App() {
+const dynamicImport = () => import('./showDynamicLog')
+
+ function App() {
+    dynamicImport().then(imp => {
+        imp.default()
+    })
   return (
     <div className="App">
       <p>App.js</p>
+        <p>new change</p>
     </div>
   );
 }
